e628c0ebaaf115a4f6d243b047e68fadfdd08b08
Caelan <caelan.curry@gmail.com>
2022-09-15 16:24:20 +0200

    Updated auth middleware

--- middleware.js
+++ middleware.js
@@ -4,20 +4,16 @@ import { NextResponse } from "next/server";
 export async function middleware(req) {
   //Token will exist if user logged in
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
+  const { pathname } = req.nextUrl;
+  console.log(pathname);
 
-  if (token) {
-    // Signed in
-    console.log("JSON Web Token accepted, redirecting to home");
-    if (req.nextUrl.pathname !== "/") {
-      return NextResponse.redirect(new URL("/", req.url));
-    }
-  } else {
-    // Not Signed in
-    console.log("No token, redirecting to login");
+  // Invalid token, don't allow access to protected routes. NEXT Middleware requires path to be defined, simply checking for !token doesn't work
+  if (!token && pathname === "/") {
     return NextResponse.redirect(new URL("/login", req.url));
   }
-}
 
-export const config = {
-  matcher: "/",
-};
+  // Active token, redirect if /login accessed
+  if (token && pathname.startsWith("/login")) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+}
