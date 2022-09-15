bc2f4240cceea4a89b8a8c8d05f2ec291d5d1b7e
Caelan <caelan.curry@gmail.com>
2022-09-15 16:25:49 +0200

    Updated auth middleware

--- middleware.js
+++ middleware.js
@@ -5,7 +5,6 @@ export async function middleware(req) {
   //Token will exist if user logged in
   const token = await getToken({ req, secret: process.env.JWT_SECRET });
   const { pathname } = req.nextUrl;
-  console.log(pathname);
 
   // Invalid token, don't allow access to protected routes. NEXT Middleware requires path to be defined, simply checking for !token doesn't work
   if (!token && pathname === "/") {
