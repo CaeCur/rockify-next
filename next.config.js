5f29d21b3073e9f2361c3f1afabb72fdee494fd1
Caelan <caelan.curry@gmail.com>
2022-09-16 17:43:56 +0200

    Implement Recoil state management

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { domains: ["i.scdn.co"] },
+  images: { domains: ["i.scdn.co", "mosaic.scdn.co"] },
 };
 
 module.exports = nextConfig;
