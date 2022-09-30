e18133ffcc3bdaa2ec163d6bf21559be2380a0d2
Caelan <caelan.curry@gmail.com>
2022-09-30 16:46:52 +0200

    FIXED first load issue. session was not passed

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <RecoilRoot>
         <Component {...pageProps} />
       </RecoilRoot>
