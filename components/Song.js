642aab6b369e428c517273d44472757b115def76
Caelan <caelan.curry@gmail.com>
2022-09-30 07:23:08 +0200

    Building song interaction

--- components/Song.js
+++ components/Song.js
@@ -1,12 +1,28 @@
 import Image from "next/image";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import useSpotify from "../hooks/useSpotify";
 import { msToMinutesAndSeconds } from "../lib/time";
 
 export const Song = ({ order, track }) => {
+  const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+  const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
+
+  const playSong = () => {
+    setCurrentTrackId(track.track.id);
+    setIsPlaying(true);
+    spotifyApi.play({
+      uris: [track.track.uri],
+    });
+  };
+
   const spotifyApi = useSpotify();
 
   return (
-    <div className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer">
+    <div
+      className="grid grid-cols-2 text-gray-500 py-4 px-5 hover:bg-gray-900 rounded-lg cursor-pointer"
+      onClick={playSong}
+    >
       <div className="flex items-center space-x-4">
         <p>{order + 1}</p>
         <div className="relative h-10 w-10">
